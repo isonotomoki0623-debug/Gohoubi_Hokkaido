0eb1dd9290b196a274b065832704e15ee2338b46
shunnosuke nakamura~ <komesan0324@gmail.com>
2026-05-14 15:22:19 +0900

    sqlの確認

---
+++ src/main/resources/templates/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO users(name, email , password , level, prefecture_id, job_id, role) VALUES
+('尊さん', takeru@example.com ,Takeru123, 1 , 1, 1 , 'user');
+
+INSERT INTO users(name, email , password , level, prefecture_id, job_id, role) VALUES
+('ゆきちゃん', yuki@example.com ,Yuki123, 99 , 1, 2 , 'user');
+
+INSERT INTO users(name, email , password , level, prefecture_id, job_id, role) VALUES
+('ゆりか', yurika@example.com ,yurika123, 10 , 1, 3 , 'user');
+
+INSERT INTO users(name, email , password , level, prefecture_id, job_id, role) VALUES
+('磯野倫希', tomoki@example.com ,Tomoki123, 50 , 1, 2 , 'admin');
+
+INSERT INTO users(name, email , password , level, prefecture_id, job_id, role) VALUES
+('しゅん', shun@example.com ,Shun123, 5, 2, 1 , 'user');
