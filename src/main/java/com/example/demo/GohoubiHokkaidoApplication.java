f7c25e30fca458d80601f2f6c7a2329de9684ca5
Isono Tomoki <isono.tomoki0623@gmail.com>
2026-05-14 14:00:14 +0900

    開発環境の作成

---
+++ src/main/java/com/example/demo/GohoubiHokkaidoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GohoubiHokkaidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GohoubiHokkaidoApplication.class, args);
+	}
+
+}
