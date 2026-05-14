e7c53c67736294f5ef2a6f522e6caacdb653aa3c
shunnosuke nakamura~ <komesan0324@gmail.com>
2026-05-14 16:19:17 +0900

    並び替え修正

--- src/main/resources/templates/data.sql
+++ src/main/resources/templates/data.sql
@@ -1,14 +1,14 @@
-INSERT INTO users(name, email , password , level, prefecture_id, job_id, role) VALUES
-('尊さん', takeru@example.com ,Takeru123, 1 , 1, 1 , 'User');
+INSERT INTO users(name, password  , email , job_id,level, prefecture_id,  role) VALUES
+('尊さん',Takeru123,Takeru123takeru@example.com , 1 , 1, 1 , 'User');
 
-INSERT INTO users(name, email , password , level, prefecture_id, job_id, role) VALUES
-('ゆきちゃん', yuki@example.com ,Yuki123, 99 , 1, 2 , 'User');
+INSERT INTO users(name, password , email , job_id, level, prefecture_id, role) VALUES
+('ゆきちゃん', Yuki123,yuki@example.com , 2 , 99, 1 , 'User');
 
-INSERT INTO users(name, email , password , level, prefecture_id, job_id, role) VALUES
-('ゆりか', yurika@example.com ,yurika123, 10 , 1, 3 , 'User');
+INSERT INTO users(name, password , email , job_id, level, prefecture_id, role) VALUES
+('ゆりか', yurika123,yurika@example.com , 3 , 10, 1 , 'User');
 
-INSERT INTO users(name, email , password , level, prefecture_id, job_id, role) VALUES
-('磯野倫希', tomoki@example.com ,Tomoki123, 50 , 1, 2 , 'Admin');
+INSERT INTO users(name, password , email , job_id, level, prefecture_id, role) VALUES
+('磯野倫希', Tomoki123,tomoki@example.com , 2 , 5, 1 , 'Admin');
 
-INSERT INTO users(name, email , password , level, prefecture_id, job_id, role) VALUES
-('しゅん', shun@example.com ,Shun123, 5, 2, 1 , 'User');
+INSERT INTO users(name, password , email , job_id, level, prefecture_id, role) VALUES
+('しゅん', Shun123,shun@example.com , 1, 2, 2 , 'User');
