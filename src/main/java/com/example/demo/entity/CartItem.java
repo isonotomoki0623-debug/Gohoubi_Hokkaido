004e85cd8be8979b4d6e393bee943ea958a0bba0
Isono Tomoki <isono.tomoki0623@gmail.com>
2026-05-18 10:39:32 +0900

    cart追加機能の作成

--- src/main/java/com/example/demo/entity/CartItem.java
+++ src/main/java/com/example/demo/entity/CartItem.java
@@ -7,20 +7,28 @@ public class CartItem {
 	private int productId;
 	private String name;
 	private int price;
-	private String categoryId;
-	private String areaId;
+	private int categoryId;
+	private int areaId;
 	private int quantity;
 
 	public CartItem() {
 	}
 
-	public CartItem(int productId, String name, int price, String categoryId, String areaId, int quantity) {
+	public CartItem(int productId, String name, int price, int categoryId, int areaId) {
 		this.productId = productId;
 		this.name = name;
 		this.price = price;
 		this.categoryId = categoryId;
 		this.areaId = areaId;
-		this.quantity = quantity;
+		this.quantity = 1;
+	}
+
+	public void incrementQuantity() {
+		this.quantity++;
+	}
+
+	public int getSubtotal() {
+		return price * quantity;
 	}
 
 }
