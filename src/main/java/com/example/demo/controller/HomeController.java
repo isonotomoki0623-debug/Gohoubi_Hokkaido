2f2c2486b76afbf4de5d024d74b23e34ccf69cd7
Yurika Hirai <youlihuapingjing@gmail.com>
2026-05-19 14:34:59 +0900

    ユーザーランキング機能を実装（動作未確認）

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.entity.Product;
+import com.example.demo.entity.User;
 import com.example.demo.mapper.ProductMapper;
 import com.example.demo.mapper.UserMapper;
 
@@ -17,11 +18,10 @@ public class HomeController {
 	private UserMapper userMapper;
 
 	//マッパーインターフェースをインスタンス化
-	public HomeController(ProductMapper productMapper) {
-		this.productMapper = productMapper;
-	}
+	public HomeController(ProductMapper productMapper,
+			UserMapper userMapper) {
 
-	public HomeController(UserMapper userMapper) {
+		this.productMapper = productMapper;
 		this.userMapper = userMapper;
 	}
 
@@ -39,9 +39,9 @@ public class HomeController {
 		model.addAttribute("rankingProducts", rankingProducts);
 
 		//ユーザーの購入金額ランキングを取得
-		List<Product> rankingUsers = userMapper.findUsersTop3();
+		List<User> rankingUsers = userMapper.findUsersTop3();
 
-		model.addAttribute("rankingProducts", rankingProducts);
+		model.addAttribute("rankingUsers", rankingUsers);
 
 		return "home";
 	}
