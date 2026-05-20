fb7e6c5d602add948b20dcb62aa974f58dc9c65d
yuki <y.suga0117@gmail.com>
2026-05-20 10:02:08 +0900

    クーポン名を取得

--- src/main/java/com/example/demo/entity/Order.java
+++ src/main/java/com/example/demo/entity/Order.java
@@ -12,4 +12,14 @@ public class Order {
 	private int totalAmount;
 	private boolean status;
 	private int couponId;
+	
+    private String couponName;
+ // getter / setter も追加
+    public String getCouponName() {
+        return couponName;
+    }
+
+    public void setCouponName(String couponName) {
+        this.couponName = couponName;
+    }
 }
